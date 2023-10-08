5f0d2a6da9d43a2cc216aea1b8a0f9d3199e82d0
diego <diego@LAPTOP-88NOVFNM>
2023-10-07 20:52:22 -0500

    correecion

---
+++ src/main/java/com/mycompany/tarea02/Tarea02.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea02;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author diego
+ */
+public class Tarea02 {
+
+    public static void main(String[] args) {
+        Scanner data =new Scanner(System.in);
+      double nota1;
+        System.out.println("ingrese nota del alumno");
+        nota1= data.nextDouble();
+        
+    if(nota1>=60){
+        System.out.println("APROBADO");
+    }else
+        System.out.println("DESAPROBADO");
+    }
+        
+    }
+        
+
